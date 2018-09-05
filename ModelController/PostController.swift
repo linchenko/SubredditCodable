aec38ea7b4457aff201326dc27616290c19913c0
Levi Linchenko <levilinchenko@Levis-MacBook-Pro.local>
2018-09-05 07:39:03 -0600

    Perces reddit contect, updates UIViews

---
+++ ModelController/PostController.swift
@@ -0,0 +1,9 @@
+//
+//  PostController.swift
+//  SubredditCodable
+//
+//  Created by Levi Linchenko on 04/09/2018.
+//  Copyright © 2018 Levi Linchenko. All rights reserved.
+//
+
+import Foundation
